b1e4199447c7ee749ed76b046b42ee983dd5c6d7
Timmer2021 <80185844+Timmer2021@users.noreply.github.com>
2026-08-31 15:36:56 -0400

    Update socket.js

--- routes/socket.js
+++ routes/socket.js
@@ -6,10 +6,9 @@ var _ = require('lodash');
 var jsonfile = require('jsonfile');
 var id, datas = {};
 
-module.exports = function (io) {
+module.exports = function (io, app) {
   return function (socket) {
     socket.on('game:start', function (data) {
-      console.log('game:start ' + data.data.id);
       id = data.data.id;
       datas[id] = data;
       data.game.round = 'J';
@@ -17,11 +16,8 @@ module.exports = function (io) {
     });
 
     socket.on('round:end', function (data) {
-      console.log('round:end ' + data.round);
       if (data.round === 'J') {
         data.round = 'DJ';
-        
-        // Find the lowest scoring player
         var lowestScore = 0;
         for(var i = 1; i <= 5; i++) {
           var key = "player_" + i;
@@ -35,7 +31,6 @@ module.exports = function (io) {
             }
           }
         }
-
       }
       else if (data.round === 'DJ') {
         data.round = 'FJ';
@@ -43,7 +38,6 @@ module.exports = function (io) {
       }
       else if (data.round === 'FJ') {
         data.round = 'end';
-
         var file = 'games/' + id + '-' + new Date().getTime() + '.json';
         jsonfile.writeFileSync(file, data, { spaces: 2 });
       }
@@ -52,29 +46,38 @@ module.exports = function (io) {
     })
 
     socket.on('board:init', function () {
-      console.log('board:init');
       socket.emit('board:init', datas[id]);
     });
 
     socket.on('game:init', function (data) {
-      console.log('game:init ' + data);
       socket.emit('game:init', datas[data]);
     });
 
     socket.on('clue:start', function (data) {
-      console.log('clue:start ' + data);
       socket.broadcast.emit('clue:start', data);
     });
 
     socket.on('clue:daily', function (data) {
-      console.log('clue:daily');
       socket.broadcast.emit('clue:daily', data);
     });
 
     socket.on('clue:end', function (data) {
-      console.log('clue:end');
       datas[id].game = data;
       socket.broadcast.emit('clue:end', data);
+    });
+
+    socket.on('buzzers:arm', function (data) {
+      io.emit('buzzers:arm', data);
+    });
+
+    socket.on('buzzer:verdict', function (data) {
+      console.log('SERVER RECEIVED buzzer:verdict ->', JSON.stringify(data));
+      io.emit('buzzer:verdict', data);
+    });
+
+    socket.on('board:verdict', function (data) {
+      console.log('SERVER RECEIVED board:verdict ->', JSON.stringify(data));
+      io.emit('board:verdict', data);
     });
   };
 };
